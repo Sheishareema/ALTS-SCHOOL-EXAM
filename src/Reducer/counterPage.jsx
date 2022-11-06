8123b9765284f5c4d7bbada858a9483380d2aab5
Ameerah <sheisameerah@gmail.com>
2022-11-06 21:27:15 +0000

    built the app

---
+++ src/Reducer/counterPage.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import useCounterHook from './counterHook';
+
+const CounterPage = () => {
+    const { count, increment, decrement, reset } = useCounterHook();
+    return (
+      <div className="flex-center">
+        <div className='flex-column'>
+            <h1>Counter App</h1>
+          <p>{count}</p>
+          <button onClick={increment} className="butt">Increment</button>
+          <button onClick={decrement} className="butt">Decrement</button>
+          <button onClick={reset} className="butt">Reset</button>
+        </div>
+      </div>
+    );
+}
+
+export default CounterPage
