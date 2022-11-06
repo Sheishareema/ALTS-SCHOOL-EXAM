e2317442dca69bfc7fbf5c276a07fd3f9cd7c24f
Ameerah <sheisameerah@gmail.com>
2022-11-06 19:46:34 +0000

    first commit

---
+++ counter/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import CounterPage from './Reducer/counterPage';
+import "./index.css";
+//import ErrorBoundary from './ErrorBoundary';
+
+
+function App() {
+  return (
+    // <ErrorBoundary>
+    <CounterPage />
+    // </ErrorBoundary>
+  );
+}
+
+export default App;
